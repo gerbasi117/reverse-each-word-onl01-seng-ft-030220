5b5cb0a7930c2f7f655717454dbe6937d6b9df59
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-16 02:29:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,5 @@
-def each_word(string)
-array = string.split(" ")
-  array_two = []
-  array.each do|string|
-    array_two << string.reverse
-  end
-  array_two.join(" ")
-end
-
-def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
+def reverse_each_word(string)
+  words_array = string.split(" ")
   new_array = []
   new_array = words_array.collect {|word| word.reverse }
   new_array.join(" ")
