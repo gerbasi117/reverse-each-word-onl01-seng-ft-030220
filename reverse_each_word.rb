37376e3666380d0b9c6915e371d9b836bd1e40c9
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-16 02:28:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,9 @@ array = string.split(" ")
   array_two.join(" ")
 end
 
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
+end
